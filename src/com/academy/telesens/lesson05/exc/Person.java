e5eeca468e4758b0a5eb78f749d3dbcd443da48b
af_oleg <oleg.kh81@gmail.com>
2020-12-10 20:42:45 +0200

    Lesson 05 - Exc Person

---
+++ src/com/academy/telesens/lesson05/exc/Person.java
@@ -0,0 +1,4 @@
+package com.academy.telesens.lesson05.exc;
+
+public class Person {
+}
---
+++ src/com/academy/telesens/lesson05/exc/PersonDemo.java
@@ -0,0 +1,8 @@
+package com.academy.telesens.lesson05.exc;
+
+public class PersonDemo {
+    public static void main(String[] args) {
+        Person person = new Person();
+
+    }
+}
